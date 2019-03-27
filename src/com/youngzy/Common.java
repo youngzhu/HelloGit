6c9883fee6d645064d36f2edcdc678a1c795df2b
Young-Mac <young.zhu.2012@gmail.com>
2019-03-27 21:55:21 +0800

    项目初始化

---
+++ src/com/youngzy/Common.java
@@ -0,0 +1,17 @@
+package com.youngzy;
+
+/**
+ * 公用类
+ *
+ * @author by youngzy
+ * @version 1.0
+ * 
+ * Create Time: Mar 27, 2019
+ * Package&FileName: com.youngzy.Common
+ */
+public class Common {
+
+	public void doSomething() {
+
+	}
+}
